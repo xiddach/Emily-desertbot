d2a70c48ec8e6b9f460a2900ffcc05edf2d600a3
xiddach <anton.zaharchuk98@gmail.com>
2025-01-29 19:04:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -155,7 +155,7 @@ def cancel(update: Update, context: CallbackContext) -> int:
 
 def main() -> None:
     # Токен вашого Telegram-бота
-    TOKEN = os.environ.get("7631205077:AAFt7ryCShzyBA43ou-7IldEWGsAO0TyB9E")
+    TOKEN = os.environ.get('7631205077:AAFt7ryCShzyBA43ou-7IldEWGsAO0TyB9E')
     
     # Створюємо Updater і передаємо йому токен бота
     updater = Updater(TOKEN)
